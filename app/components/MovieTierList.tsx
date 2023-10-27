5cb0470f753582b8d429b559274331e9c126cfb8
chrisreylo73 <chrisreylo73@gmail.com>
2023-10-27 17:01:06 -0600

    filterered movies

--- app/components/MovieTierList.tsx
+++ app/components/MovieTierList.tsx
@@ -1,47 +1,65 @@
 
 import styles from "app/styles/MovieTierList.css";
 import { Link, useLoaderData, useLocation, useParams } from '@remix-run/react';
-import { LoaderFunction } from '@remix-run/node';
-import { PrismaClient } from "@prisma/client";
 import { Movie } from "@prisma/client";
 
 export function links() {
 	return [{ rel: "stylesheet", href: styles }];
 }
 
-const MovieTierList = ({movies}: {movies: Movie[]}) => {
+const MovieTierList = ({ categorizedMovies }: { categorizedMovies: Record<string, Movie[]> })  => {
   return (
     <div>
-      {/* {movies.map((movie) => ( */}
-        <div>
        <div className="tiers">
             <div className="S">
               <p className="tierLabel">S</p>
-              {movies.map((movie) => (
+              {categorizedMovies['S'].map((movie) => (
               <div className="movie">
                  <div className="del"></div>
-                 <p className="movieTitle">The Dark Knight</p>
+                 <p className="movieTitle">{movie.title}</p>
                 </div>
                ))}
             </div>
             <div className="A">
-            <p className="tierLabel">A</p>
+          <p className="tierLabel">A</p>
+          {categorizedMovies['A'].map((movie) => (
+              <div className="movie">
+                 <div className="del"></div>
+                 <p className="movieTitle">{movie.title}</p>
+                </div>
+               ))}
             </div>
             <div className="B">
-            <p className="tierLabel">B</p>
+          <p className="tierLabel">B</p>
+          {categorizedMovies['B'].map((movie) => (
+              <div className="movie">
+                 <div className="del"></div>
+                 <p className="movieTitle">{movie.title}</p>
+                </div>
+               ))}
             </div>
             <div className="C">
-            <p className="tierLabel">C</p>
+          <p className="tierLabel">C</p>
+          {categorizedMovies['C'].map((movie) => (
+              <div className="movie">
+                 <div className="del"></div>
+                 <p className="movieTitle">{movie.title}</p>
+                </div>
+               ))}
             </div>
             <div className="D">
-               <p className="tierLabel">D</p>
+          <p className="tierLabel">D</p>
+          {categorizedMovies['D'].map((movie) => (
+              <div className="movie">
+                 <div className="del"></div>
+                 <p className="movieTitle">{movie.title}</p>
+                </div>
+               ))}
             </div>
         </div>
         <Link to={`./add_movie`}>
           <button style={{ borderRadius: "5px 5px 15px 15px" }}>ADD MOVIE</button>
         </Link>
-        </div>
-       {/* ))} */}
     </div>
   )
 }
--- app/routes/movies.$id.tsx
+++ app/routes/movies.$id.tsx
@@ -6,29 +6,34 @@ export function links() {
   return [...MovieTierLinks()];
 }
 
-
 export async function loader() {
    // Create a new instance of PrismaClient
    const prisma = new PrismaClient();
    try {
      // Fetch data from the database
-     const allMovies = await prisma.movie.findMany();
-     console.log("allMovies:", allMovies);
+     const movies = await prisma.movie.findMany();
+      console.log("allMovies:", movies);
+      const categorizedMovies = {
+         S: movies.filter((movie) => movie.overall === 5),
+         A: movies.filter((movie) => movie.overall === 4),
+         B: movies.filter((movie) => movie.overall === 3),
+         C: movies.filter((movie) => movie.overall === 2),
+         D: movies.filter((movie) => movie.overall === 1),
+       };
      // Return the fetched data
-     return allMovies;
+     return categorizedMovies;
    } finally {
      // Disconnect from the database, even if there is an error
      await prisma.$disconnect();
    }
- }
+}
  
-
 export default function MoviesById() { 
-   const movies = useLoaderData()
+   const categorizedMovies = useLoaderData()
    return (
       <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
          <h1>MOVIES</h1>
-         <MovieTierList movies={movies}/>
+         <MovieTierList categorizedMovies={categorizedMovies}/>
       </div>
    );
 }
